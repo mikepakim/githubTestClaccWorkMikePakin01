4be94ad184d6ddbc27602469a09caecdae2e8b0c
mikepakim <mikepakin1@gmail.com>
2023-03-05 18:57:22 +0200

    dsafdsaf

---
+++ src/GithubTClassWork1/GithubClassWork1.java
@@ -0,0 +1,10 @@
+package GithubTClassWork1;
+
+public class GithubClassWork1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        // test gid
+        //sgfgfh
+
+    }
+}
